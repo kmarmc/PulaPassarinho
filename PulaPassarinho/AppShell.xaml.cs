69653e44a1e8f2faff0fbb7ab0720196913daa27
PulaPassarinho <kauemroberto@gmail.com>
2024-10-08 11:52:02 -0300

    pagarpraver

---
+++ PulaPassarinho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PulaPassarinho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PulaPassarinho/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PulaPassarinho;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
