8973a07170bbc05d05e50225a67ff452567ea3f8
PulaPassarinho <kauemroberto@gmail.com>
2024-10-10 08:49:46 -0300

    guiticonfigui

--- PulaPassarinho/MainPage.xaml.cs
+++ PulaPassarinho/MainPage.xaml.cs
@@ -2,11 +2,29 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	const int gravidade = 30; //pixel
+	const int TempoEntreFrames = 100; //ms
+	bool Morreu = false;
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+	void AplicarGravidade ()
+	{
+		alexandrepato.TranslationY -= gravidade;
+	}
+	async Task Desenhar ()
+	{
+		while (!Morreu)
+		{
+			AplicarGravidade (); 
+			await Task.Delay (TempoEntreFrames);
+		}
+	}
+	protected override void OnAppearing ()
+	{
+		base.OnAppearing ();
+		Desenhar();
 	}
 }
 
